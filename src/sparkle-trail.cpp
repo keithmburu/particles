215dd3509d3916c00d8dde2522abfca1b9ed37a9
kmburu <keithmburu1@gmail.com>
2023-03-28 17:41:51 -0400

    nan error

--- src/sparkle-trail.cpp
+++ src/sparkle-trail.cpp
@@ -14,8 +14,10 @@ struct Particle {
   glm::vec3 pos;
   glm::vec3 vel;
   glm::vec4 color;
-  float rot;
   float size;
+  float rot;
+  float startTime;
+  float lifetime;
 };
 
 class Viewer : public Window {
@@ -24,33 +26,84 @@ public:
   }
 
   void setup() {
-    setWindowSize(1000, 1000);
-    createConfetti(500);
+    setWindowSize(500, 500);
+    createSparkleTrail();
     renderer.setDepthTest(false);
     renderer.blendMode(agl::ADD);
   }
 
-  void createConfetti(int size)
+void createSparkleTrail()
   {
-    renderer.loadTexture("particle", "../textures/star4.png", 0);
-    for (int i = 0; i < size; i++)
-    {
+    for (int i = 1; i < _numParticles; i++) {
+      renderer.loadTexture("particle", "../textures/particle.png", 0);
       Particle particle;
-      particle.color = vec4(agl::randomUnitCube(), 1);
-      particle.size = 0.25;
+      // particle.pos = vec3(0.75 * width(), 0.75 * height(), 0);
+      int time = elapsedTime();
+      particle.pos = vec3(cos(time), sin(time), 0);
+      vec3 nextPos = vec3(cos(time + 1), sin(time + 1), 0);
+      if (i == 1) {
+        particle.vel = nextPos - particle.pos;
+      } else {
+        particle.vel = particle.pos - nextPos;
+      }
+      particle.color = vec4(1, 1, 1, 1);
+      particle.size = 0.5;
       particle.rot = 0.0;
-      particle.pos = agl::randomUnitCube();
-      particle.vel = agl::randomUnitCube();
+      particle.startTime = elapsedTime();
       mParticles.push_back(particle);
     }
   }
 
-  void updateConfetti()
+  void updateSparkleTrail()
   {
+    for (int i = 1; i < mParticles.size(); i++) {
+      Particle particle = mParticles[i];
+      if (particle.color.w > 0) {
+        particle.pos += particle.vel;
+        particle.color -= vec4(0, 0, 0, 0.1);
+        particle.size += 0.05;
+        particle.rot += 5.0;
+      } else {
+        particle.pos = mParticles[0].pos;
+        particle.vel = -mParticles[0].vel;
+        particle.color = vec4(1, 1, 1, 1);
+        particle.size = 0.5;
+        particle.rot = 0.0;
+        particle.startTime = elapsedTime();
+      }
+      mParticles[i] = particle;
+    }
+
+    if (int(elapsedTime()) % 2 == 0) {
+      cout << "pos " << mParticles[0].pos.x << " " << mParticles[0].pos.y << " vel " << mParticles[0].vel.x << " " << mParticles[0].vel.y << " alpha " << mParticles[0].color.w << endl;
+      cout << "pos " << mParticles[9].pos.x << " " << mParticles[9].pos.y << " vel " << mParticles[9].vel.x << " " << mParticles[9].vel.y << " alpha " << mParticles[9].color.w << endl;
+    }
+
+    int time = elapsedTime();
+    vec3 nextPos = vec3(cos(time + 1), sin(time + 1), 0);
+    mParticles[0].vel = nextPos - mParticles[0].pos;
+    mParticles[0].pos += mParticles[0].vel;
   }
 
-  void drawConfetti()
+  void drawSparkleTrail()
   {
+    vec3 cameraPos = renderer.cameraPosition();
+
+    // sort
+    for (int i = 1; i < mParticles.size(); i++)
+    {
+      Particle particle1 = mParticles[i];
+      Particle particle2 = mParticles[i - 1];
+      float dSqr1 = length2(particle1.pos - cameraPos);
+      float dSqr2 = length2(particle2.pos - cameraPos);
+      if (dSqr2 < dSqr1)
+      {
+        mParticles[i] = particle2;
+        mParticles[i - 1] = particle1;
+      }
+    }
+
+    // draw
     renderer.texture("image", "particle");
     for (int i = 0; i < mParticles.size(); i++)
     {
@@ -69,7 +122,7 @@ public:
   }
 
   void scroll(float dx, float dy) {
-    eyePos.z += dy;
+    eyePos.x += dy;
   }
 
   void keyUp(int key, int mods) {
@@ -82,20 +135,20 @@ public:
     renderer.perspective(glm::radians(60.0f), aspect, 0.1f, 50.0f);
 
     renderer.lookAt(eyePos, lookPos, up);
-    renderer.sprite(position, vec4(1.0f), 0.25f);
-    updateConfetti();
-    drawConfetti();
+    updateSparkleTrail();
+    drawSparkleTrail();
+
     renderer.endShader();
   }
 
 protected:
 
-  vec3 eyePos = vec3(0, 0, 3);
+  vec3 eyePos = vec3(0, 0, 10);
   vec3 lookPos = vec3(0, 0, 0);
   vec3 up = vec3(0, 1, 0);
-  vec3 position = vec3(1, 0, 0);
 
   std::vector<Particle> mParticles;
+  int _numParticles = 10;
 };
 
 int main(int argc, char** argv)
