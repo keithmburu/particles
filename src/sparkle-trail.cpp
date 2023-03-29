6855f5613f18e2b36e7e1cd31bcfce32ebc6e036
kmburu <keithmburu1@gmail.com>
2023-03-29 14:10:36 -0400

    working but trail is discontinuous

--- src/sparkle-trail.cpp
+++ src/sparkle-trail.cpp
@@ -4,7 +4,9 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "agl/window.h"
+#include "unistd.h"
 
 using namespace std;
 using namespace glm;
@@ -23,33 +25,38 @@ struct Particle {
 class Viewer : public Window {
 public:
   Viewer() : Window() {
+    _width = _height = 500;
+    // _radius = std::min(_width, _height) * 0.75;
+    _radius = 20;
+    // eyePos = vec3(0, 0, std::min(_width, _height));
+    srand(elapsedTime());
   }
 
   void setup() {
-    setWindowSize(500, 500);
+    setWindowSize(_width, _height);
     createSparkleTrail();
     renderer.setDepthTest(false);
     renderer.blendMode(agl::ADD);
+    renderer.loadTexture("star", "../textures/star4.png", 0);
   }
 
 void createSparkleTrail()
   {
-    for (int i = 1; i < _numParticles; i++) {
-      renderer.loadTexture("particle", "../textures/particle.png", 0);
+    for (int i = 0; i < _numParticles; i++) {
       Particle particle;
-      // particle.pos = vec3(0.75 * width(), 0.75 * height(), 0);
-      int time = elapsedTime();
-      particle.pos = vec3(cos(time), sin(time), 0);
-      vec3 nextPos = vec3(cos(time + 1), sin(time + 1), 0);
-      if (i == 1) {
+      float time = elapsedTime();
+      particle.pos = vec3(_radius * cos(time), _radius * sin(time), 0);
+      vec3 nextPos = vec3(_radius * cos(time + dt()), _radius * sin(time + dt()), 0);
+      if (i == 0) {
         particle.vel = nextPos - particle.pos;
+        particle.color = vec4(1, 1, 1, 1);
       } else {
         particle.vel = particle.pos - nextPos;
+        particle.color = vec4(1, 1, 1, (rand() % 11) / 10.0f);
       }
-      particle.color = vec4(1, 1, 1, 1);
-      particle.size = 0.5;
+      particle.size = 2.0;
       particle.rot = 0.0;
-      particle.startTime = elapsedTime();
+      particle.startTime = time;
       mParticles.push_back(particle);
     }
   }
@@ -60,51 +67,37 @@ void createSparkleTrail()
       Particle particle = mParticles[i];
       if (particle.color.w > 0) {
         particle.pos += particle.vel;
-        particle.color -= vec4(0, 0, 0, 0.1);
-        particle.size += 0.05;
-        particle.rot += 5.0;
+        particle.color -= vec4((rand() % 6)/255.0f, (rand() % 6)/255.0f, (rand() % 5)/255.0f, 0.001);
+        particle.size += 0.001;
+        particle.rot += 0.005;
       } else {
         particle.pos = mParticles[0].pos;
         particle.vel = -mParticles[0].vel;
-        particle.color = vec4(1, 1, 1, 1);
-        particle.size = 0.5;
+        particle.color = vec4(1, 1, 1, (rand() % 11) / 10.0f);
+        particle.size = 2.0;
         particle.rot = 0.0;
-        particle.startTime = elapsedTime();
       }
       mParticles[i] = particle;
     }
 
-    if (int(elapsedTime()) % 2 == 0) {
-      cout << "pos " << mParticles[0].pos.x << " " << mParticles[0].pos.y << " vel " << mParticles[0].vel.x << " " << mParticles[0].vel.y << " alpha " << mParticles[0].color.w << endl;
-      cout << "pos " << mParticles[9].pos.x << " " << mParticles[9].pos.y << " vel " << mParticles[9].vel.x << " " << mParticles[9].vel.y << " alpha " << mParticles[9].color.w << endl;
+    int frame = int(elapsedTime() / dt());
+    if (frame != prevFrame) {
+      prevFrame = frame;
+      for (int i = 0; i < 2; i++) {
+        cout << "pos " << mParticles[i].pos.x << " " << mParticles[i].pos.y << " | vel " << mParticles[i].vel.x << " " << mParticles[i].vel.y << " | alpha " << mParticles[i].color.w << endl;
+      }
+      cout << endl;
     }
 
-    int time = elapsedTime();
-    vec3 nextPos = vec3(cos(time + 1), sin(time + 1), 0);
-    mParticles[0].vel = nextPos - mParticles[0].pos;
     mParticles[0].pos += mParticles[0].vel;
+    float time = elapsedTime();
+    vec3 nextPos = vec3(_radius * cos(time + dt()), _radius * sin(time + dt()), 0);
+    mParticles[0].vel = nextPos - mParticles[0].pos;
   }
 
   void drawSparkleTrail()
   {
-    vec3 cameraPos = renderer.cameraPosition();
-
-    // sort
-    for (int i = 1; i < mParticles.size(); i++)
-    {
-      Particle particle1 = mParticles[i];
-      Particle particle2 = mParticles[i - 1];
-      float dSqr1 = length2(particle1.pos - cameraPos);
-      float dSqr2 = length2(particle2.pos - cameraPos);
-      if (dSqr2 < dSqr1)
-      {
-        mParticles[i] = particle2;
-        mParticles[i - 1] = particle1;
-      }
-    }
-
-    // draw
-    renderer.texture("image", "particle");
+    renderer.texture("image", "star");
     for (int i = 0; i < mParticles.size(); i++)
     {
       Particle particle = mParticles[i];
@@ -143,12 +136,17 @@ void createSparkleTrail()
 
 protected:
 
-  vec3 eyePos = vec3(0, 0, 10);
+  vec3 eyePos = vec3(0, 0, 40);
+  // vec3 eyePos;
   vec3 lookPos = vec3(0, 0, 0);
   vec3 up = vec3(0, 1, 0);
 
   std::vector<Particle> mParticles;
-  int _numParticles = 10;
+  int _numParticles = 100;
+  int _width;
+  int _height;
+  int _radius;
+  int prevFrame = 0;
 };
 
 int main(int argc, char** argv)
